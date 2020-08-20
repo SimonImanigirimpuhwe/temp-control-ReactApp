f2021006d8c233721ae8decc2265d10a82c2fba9
Simon97i <softimag08@gmail.com>
2020-08-20 09:27:38 +0200

    Updates

---
+++ precache-manifest.d6625330b5ef913dd252e7a4b4593431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209b97d8dcf9e0d43139246fa579cd99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "984ed6dc2b3e4ad0214c",
+    "url": "/static/css/main.40a2442d.chunk.css"
+  },
+  {
+    "revision": "b3d1f752bb7a0c1061be",
+    "url": "/static/js/2.3d8eedea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3d8eedea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984ed6dc2b3e4ad0214c",
+    "url": "/static/js/main.24bc95db.chunk.js"
+  },
+  {
+    "revision": "359e1f4ea794607ddfaa",
+    "url": "/static/js/runtime-main.432eea27.js"
+  }
+]);
