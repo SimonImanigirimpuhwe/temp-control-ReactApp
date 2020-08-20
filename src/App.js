e4e941b86eeaaafd1df379e0790da748531f83e0
Simon97i <softimag08@gmail.com>
2020-08-20 08:58:09 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temperature: 10,
+      backgroundColor:'cold',
+    }
+  }
+  incrimentTemp(){
+    if (this.state.temperature === 30) return;
+    this.setState({temperature: (this.state.temperature + 1)})
+    if (this.state.temperature >= 15) {
+      this.setState({backgroundColor: 'hot'})
+    }
+  }
+  decrimentTemp() {
+    if (this.state.temperature === 0) return;
+    if (this.state.temperature < 15) {
+      this.setState({backgroundColor: 'cold'})
+    }
+    this.setState({temperature: (this.state.temperature - 1)})
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className='card-container'>
+          <div className={`circle ${this.state.backgroundColor}`} >
+            <div className='temperature'>
+              {this.state.temperature}℃
+            </div>
+          </div>
+          <div className='btn-control'>
+            <button id='increase' onClick={() => this.incrimentTemp()}>+</button>
+            <button id='decrease' onClick={() => this.decrimentTemp()}>-</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+       <div>
+           <h1>Temperature control react app</h1>
+       </div>
+    )
+}
+
+export default Header;
