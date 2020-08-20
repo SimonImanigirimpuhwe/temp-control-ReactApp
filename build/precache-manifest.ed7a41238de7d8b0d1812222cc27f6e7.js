57ae6019988112b636d6482f53428d923cf990c8
Simon97i <softimag08@gmail.com>
2020-08-20 10:11:13 +0200

    re-edit homepage url

---
+++ build/precache-manifest.ed7a41238de7d8b0d1812222cc27f6e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c14f2a1f03ed6319b29959640d46de1",
+    "url": "/temp-control-ReactApp/index.html"
+  },
+  {
+    "revision": "708d07bce0cf1c9410b5",
+    "url": "/temp-control-ReactApp/static/css/main.40a2442d.chunk.css"
+  },
+  {
+    "revision": "b3d1f752bb7a0c1061be",
+    "url": "/temp-control-ReactApp/static/js/2.3d8eedea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/temp-control-ReactApp/static/js/2.3d8eedea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "708d07bce0cf1c9410b5",
+    "url": "/temp-control-ReactApp/static/js/main.90e17e39.chunk.js"
+  },
+  {
+    "revision": "86c2106a161a3da62d8e",
+    "url": "/temp-control-ReactApp/static/js/runtime-main.77179f28.js"
+  }
+]);
